417434686d935c63e383f2397fa0f5edca6b55c5
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-10-17 18:44:24 +0900

    2D

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,9 +1,78 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+public enum ItemType
+{
+    Food,
+    Water,
+    Weapon,
+    Common,
+    FirstAid,
+    MentalCare
+}
+
 public class Item
 {
-    public string name = null;
-    public Image image = null;
-    public int weight = 0;
+    [SerializeField]
+    private int id = -1;
+    [SerializeField]
+    private string name = "";
+    [SerializeField]
+    private int weight = 0;
+    [SerializeField]
+    private Image iconImage = null;
+    [SerializeField]
+    private string description = "";
+    [SerializeField]
+    private float lootPercent = 10f;
+
+    #region Property
+    public int ID
+    {
+        get
+        {
+            return id;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+    public int Weight
+    {
+        get
+        {
+            return weight;
+        }
+    }
+    public Image IconImage
+    {
+        get
+        {
+            return iconImage;
+        }
+    }
+    public string Description
+    {
+        get
+        {
+            return description;
+        }
+    }
+    public float LootPercent
+    {
+        get
+        {
+            return lootPercent;
+        }
+    }
+    #endregion
+
+
+
+
 }
