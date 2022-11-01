50e6eb5c7d5d0690b31263289c73e2e87a8dd465
BIN2428 <thstjdqls2625@gmail.com>
2022-11-01 18:57:22 +0900

    손성빈 json Data 만들기

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,9 +5,8 @@ using System.IO;
 
 public class Item : MonoBehaviour
 {
-    public ItemTypes item;
-    
-    
+    public Data item;
+
     [ContextMenu("To Json Data")]
     void SaveItemDataToJson()
     {
@@ -15,39 +14,16 @@ public class Item : MonoBehaviour
         string path = Path.Combine(Application.dataPath, "/Itemdata.json");
         File.WriteAllText(path, jsonData);
     }
-
-    //[SerializeField]
-    //private int id = -1;
-    //[SerializeField]
-    //private string name = "";
-    //[SerializeField]
-    //private int weight = 0;
-    //[SerializeField]
-    //private string description = "";
-    //[SerializeField]
-    //private float lootPercent = 10f;
-
-    //public enum ItemType
-    //{
-    //      ID;
-    //    Water,
-    //    Weapon,
-    //    Common,
-    //    FirstAid,
-    //    MentalCare
-    //}
-
 }
 
 [System.Serializable]
-public class ItemTypes
+public class Data
 {
-    public string[] foodtypes;
-    public int water;
-    public int[] ID;
-    public string[] weapon;
-    public string[] Common;
-    public int FirstAid;
-    public int mentalCare;
-    public int DegreeOfInfectionLevel;
+    public string potion_name;
+    public int value;
+    public List<Character> character = new List<Character>();
 }
+
+
+
+
