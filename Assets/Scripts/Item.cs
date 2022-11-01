d086dc11a74b26d4a31456fe14ee6a84aa117166
BIN2428 <thstjdqls2625@gmail.com>
2022-11-01 18:03:14 +0900

    손성빈  data json class 구현

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,78 +1,53 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
 
-public enum ItemType
+public class Item : MonoBehaviour
 {
-    Food,
-    Water,
-    Weapon,
-    Common,
-    FirstAid,
-    MentalCare
+    public ItemTypes item;
+    
+    
+    [ContextMenu("To Json Data")]
+    void SaveItemDataToJson()
+    {
+        string jsonData = JsonUtility.ToJson(item);
+        string path = Path.Combine(Application.dataPath, "/Itemdata.json");
+        File.WriteAllText(path, jsonData);
+    }
+
+    //[SerializeField]
+    //private int id = -1;
+    //[SerializeField]
+    //private string name = "";
+    //[SerializeField]
+    //private int weight = 0;
+    //[SerializeField]
+    //private string description = "";
+    //[SerializeField]
+    //private float lootPercent = 10f;
+
+    //public enum ItemType
+    //{
+    //      ID;
+    //    Water,
+    //    Weapon,
+    //    Common,
+    //    FirstAid,
+    //    MentalCare
+    //}
+
 }
 
-public class Item
+[System.Serializable]
+public class ItemTypes
 {
-    [SerializeField]
-    private int id = -1;
-    [SerializeField]
-    private string name = "";
-    [SerializeField]
-    private int weight = 0;
-    [SerializeField]
-    private Image iconImage = null;
-    [SerializeField]
-    private string description = "";
-    [SerializeField]
-    private float lootPercent = 10f;
-
-    #region Property
-    public int ID
-    {
-        get
-        {
-            return id;
-        }
-    }
-
-    public string Name
-    {
-        get
-        {
-            return name;
-        }
-    }
-    public int Weight
-    {
-        get
-        {
-            return weight;
-        }
-    }
-    public Image IconImage
-    {
-        get
-        {
-            return iconImage;
-        }
-    }
-    public string Description
-    {
-        get
-        {
-            return description;
-        }
-    }
-    public float LootPercent
-    {
-        get
-        {
-            return lootPercent;
-        }
-    }
-    #endregion
-
-
-
-
+    public string[] foodtypes;
+    public int water;
+    public int[] ID;
+    public string[] weapon;
+    public string[] Common;
+    public int FirstAid;
+    public int mentalCare;
+    public int DegreeOfInfectionLevel;
 }
