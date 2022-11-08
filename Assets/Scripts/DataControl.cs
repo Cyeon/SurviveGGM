1fbe0e81a292f7c703c39353c31893f84889e0a2
chogunny1 <gunnychocho@gmail.com>
2022-11-08 18:11:55 +0900

    배급 버튼 수정
    배급 버튼 수정

--- Assets/Scripts/DataControl.cs
+++ Assets/Scripts/DataControl.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DataControll : MonoBehaviour
+public class DataControl : MonoBehaviour
 {
     public Slider Fslider;
     public Slider Wslider;
